275ab9ac4bfda615187fdace270a3a14d52ea573
Mir Muhammad Abidul Haq (Ahnaf) <abidulhaqahnaf@gmail.com>
2024-09-09 16:49:03 +0600

    nasa

--- song/ffmpeg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a bin directory if it doesn't exist
-mkdir -p bin
-
-# Download the FFmpeg binary
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C bin
