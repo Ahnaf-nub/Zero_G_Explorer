68dc7f5e96bdea6fd59972836b0845ec7092408b
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-09-10 13:49:29 +0600

    Update p5.js

--- p5.js
+++ p5.js
@@ -37,10 +37,10 @@ function draw() {
       let leftStickY = gamepad.axes[1]; // Get Y-axis value of left joystick (range: -1 to 1)
 
       // Map joystick input to forward speed
-      if (leftStickY < 0.5) {
-        forwardSpeed = 5;  // Move forward
-      } else if (leftStickY > -0.5) {
-        forwardSpeed = -5;  // Move backward
+      if (leftStickY < -0.5) {
+        forwardSpeed = -5;  // Move forward
+      } else if (leftStickY > 0.5) {
+        forwardSpeed = 5;  // Move backward
       } else {
         forwardSpeed = 0;  // Stop movement
       }
