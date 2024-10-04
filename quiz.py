f668b33299a02d98592e5bc5db517ae7be9816da
A. N. M. Noor <nafis.noor.202012@gmail.com>
2024-10-04 18:41:13 +0600

    modified:   quiz.py

--- quiz.py
+++ quiz.py
@@ -9,110 +9,6 @@ load_dotenv()
 
 NASA_API = os.getenv("NASA_API_KEY")
 
-
-"""quiz_today = {
-    "01":  {
-        "question": "What is the capital of France?",
-        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "answer": 0
-    },
-    "02": {
-        "question": "What is the capital of India?",
-        "options": ["Paris", "London", "New Delhi", "Madrid"],
-        "answer": 2
-    },
-    "03": {
-        "question": "What is the capital of Germany?",
-        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "answer": 2
-    },
-    "04": {
-        "question": "What is the capital of Spain?",
-        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "answer": 3
-    },
-    "05": {
-        "question": "What is the capital of USA?",
-        "options": ["Paris", "Washington DC", "Berlin", "Madrid"],
-        "answer": 1
-    },
-    "06": {
-        "question": "What is the capital of UK?",
-        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "answer": 1
-    },
-    "07": {
-        "question": "What is the capital of Australia?",
-        "options": ["Paris", "London", "Berlin", "Canberra"],
-        "answer": 3
-    },
-    "08": {
-        "question": "What is the capital of Japan?",
-        "options": ["Paris", "London", "Tokyo", "Madrid"],
-        "answer": 2
-    },
-    "09": {
-        "question": "What is the capital of China?",
-        "options": ["Paris", "Beijing", "Berlin", "Madrid"],
-        "answer": 1
-    },
-    "10": {
-        "question": "What is the capital of Russia?",
-        "options": ["Paris", "London", "Berlin", "Moscow"],
-        "answer": 3
-    },
-    "11": {
-        "question": "What is the capital of Brazil?",
-        "options": ["Paris", "London", "Berlin", "Brasilia"],
-        "answer": 3
-    },
-    "12": {
-        "question": "What is the capital of Canada?",
-        "options": ["Paris", "London", "Berlin", "Ottawa"],
-        "answer": 3
-    },
-    "13": {
-        "question": "What is the capital of Italy?",
-        "options": ["Paris", "London", "Rome", "Madrid"],
-        "answer": 2
-    },
-    "14": {
-        "question": "What is the capital of South Africa?",
-        "options": ["Paris", "London", "Berlin", "Pretoria"],
-        "answer": 3
-    },
-    "15": {
-        "question": "What is the capital of Argentina?",
-        "options": ["Paris", "London", "Berlin", "Buenos Aires"],
-        "answer": 3
-    },
-    "16": {
-        "question": "What is the capital of Mexico?",
-        "options": ["Paris", "London", "Berlin", "Mexico City"],
-        "answer": 3
-    },
-    "17": {
-        "question": "What is the capital of Egypt?",
-        "options": ["Paris", "London", "Berlin", "Cairo"],
-        "answer": 3
-    },
-    "18": {
-        "question": "What is the capital of Turkey?",
-        "options": ["Paris", "London", "Berlin", "Ankara"],
-        "answer": 3
-    },
-    "19": {
-        "question": "What is the capital of Saudi Arabia?",
-        "options": ["Paris", "London", "Berlin", "Riyadh"],
-        "answer": 3
-    },
-    "20": {
-        "question": "What is the capital of Pakistan?",
-        "options": ["Paris", "London", "Berlin", "Islamabad"],
-        "answer": 3
-    }
-}"""
-
 quiz_today = {
     "H873": {
         "question": "What type of CME was detected by STEREO A / SOHO on September 8th, 2024?",
@@ -138,6 +34,81 @@ quiz_today = {
         "question": "What is the estimated range of the maximum Kp index for the CME detected on September 8th, 2024 impacting NASA missions near Earth?",
         "options": ["0-2", "4-6", "7-9", "10-12"],
         "answer": 1
+    },
+    "U824": {
+        "question": "What type of CME was detected by SOHO on October 3rd, 2024, that is expected to impact Mars and STEREO A?",
+        "options": ["S-type", "C-type", "O-type", "R-type"],
+        "answer": 1
+    },
+    "Y359": {
+        "question": "What was the estimated speed of the CME detected on October 3rd, 2024, that is expected to impact Mars and STEREO A?",
+        "options": ["500 km/s", "646 km/s", "800 km/s", "950 km/s"],
+        "answer": 1
+    },
+    "P354": {
+        "question": "When is the leading edge of the CME detected on October 3rd, 2024, expected to reach Mars?",
+        "options": ["October 5th, 2024, 17:04Z", "October 6th, 2024, 12:02Z", "October 7th, 2024, 17:04Z", "October 8th, 2024, 12:02Z"],
+        "answer": 2
+    },
+    "O764": {
+        "question": "What is the estimated range of the maximum Kp index for the CME detected on October 3rd, 2024, that is expected to have a glancing blow at NASA missions near Earth?",
+        "options": ["0-2", "2-4", "4-6", "6-8"],
+        "answer": 1
+    },
+    "V943": {
+        "question": "What was the intensity of the flare that is associated with the CME detected on October 3rd, 2024, that is expected to impact Mars and STEREO A?",
+        "options": ["M5.7", "M6.7", "X9.0", "C8.5"],
+        "answer": 1
+    },
+    "Z319": {
+        "question": "What was the peak time of the flare associated with the CME detected on October 3rd, 2024, that is expected to impact Mars and STEREO A?",
+        "options": ["2024-10-03T20:18Z", "2024-10-03T20:23Z", "2024-10-03T20:28Z", "2024-10-03T20:33Z"],
+        "answer": 2
+    },
+    "R487": {
+        "question": "What type of CME was detected by STEREO A / SOHO on October 3rd, 2024, that is expected to impact Lucy and Solar Orbiter?",
+        "options": ["S-type", "C-type", "O-type", "R-type"],
+        "answer": 1
+    },
+    "S124": {
+        "question": "What was the estimated speed of the CME detected on October 3rd, 2024, that is expected to impact Lucy and Solar Orbiter?",
+        "options": ["550 km/s", "706 km/s", "850 km/s", "900 km/s"],
+        "answer": 1
+    },
+    "Q792": {
+        "question": "When is the leading edge of the CME detected on October 3rd, 2024, expected to reach Lucy?",
+        "options": ["October 4th, 2024, 07:02Z", "October 5th, 2024, 22:13Z", "October 6th, 2024, 07:44Z", "October 7th, 2024, 10:00Z"],
+        "answer": 2
+    },
+    "T218": {
+        "question": "What type of CME was detected by STEREO A / SOHO on October 3rd, 2024, that is expected to impact Juno, Lucy, STEREO A, Mars, and missions near Earth?",
+        "options": ["S-type", "C-type", "O-type", "R-type"],
+        "answer": 1
+    },
+    "G532": {
+        "question": "What was the estimated speed of the CME detected on October 3rd, 2024, that is expected to impact Juno, Lucy, STEREO A, Mars, and missions near Earth?",
+        "options": ["600 km/s", "750 km/s", "822 km/s", "900 km/s"],
+        "answer": 2
+    },
+    "H612": {
+        "question": "When is the leading edge of the CME detected on October 3rd, 2024, expected to reach STEREO A?",
+        "options": ["October 5th, 2024, 22:13Z", "October 6th, 2024, 03:54Z", "October 7th, 2024, 10:00Z", "October 8th, 2024, 12:02Z"],
+        "answer": 0
+    },
+    "J194": {
+        "question": "What is the estimated range of the maximum Kp index for the CME detected on October 3rd, 2024, that is expected to impact Juno, Lucy, STEREO A, Mars, and missions near Earth?",
+        "options": ["3-5", "5-7", "7-9", "9-11"],
+        "answer": 1
+    },
+    "K847": {
+        "question": "What was the intensity of the flare that is associated with the CME detected on October 3rd, 2024, that is expected to impact Juno, Lucy, STEREO A, Mars, and missions near Earth?",
+        "options": ["M6.7", "X9.0", "C8.5", "M5.0"],
+        "answer": 1
+    },
+    "L134": {
+        "question": "What was the peak time of the flare associated with the CME detected on October 3rd, 2024, that is expected to impact Juno, Lucy, STEREO A, Mars, and missions near Earth?",
+        "options": ["2024-10-03T12:18Z", "2024-10-03T12:23Z", "2024-10-03T12:28Z", "2024-10-03T12:33Z"],
+        "answer": 0
     }
 }
 
